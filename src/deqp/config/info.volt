Wallbraker/dEQP

commit 359d0e397084c08cce8fce65eeb5aa14c558ee60

deqp: Add --randomize argument

src/deqp/config/info.volt
@@ -28,6 +28,7 @@ fn printConfig(s: Settings)
 	}
 	info("\tctsBuildDir    = '%s'", s.ctsBuildDir);
 	info("\thastyBatchSize = %s%s", s.hastyBatchSize, s.hastyBatchSize == 0 ? " (smart mode)" : "");
+	info("\trandomize      = %s", s.randomize);
 	info("\tthreads        = %s", s.threads);
 	info("\tresultsFile    = '%s'", s.resultsFile);
 	info("\ttempDir        = '%s'", s.tempDir);
@@ -38,6 +39,7 @@ fn printAllArgsAndConfig()
 	printFailing();
 	printThreads();
 	printHastyBatchSize();
+	printRandomize();
 	printCtsBuildDir();
 	printTestNamesFile();
 	printResultsFile();
@@ -102,6 +104,14 @@ fn printThreads(suffix: string = ":")
 fn printHastyBatchSize(suffix: string = ":")
 {
 	info("Hasty batch size%s", suffix);
+	info("\tUse 0 for smart mode.");
+	info("\tArg:    --hasty-batch-size X");
+	info("\tConfig: hastyBatchSize=X");
+}
+
+fn printRandomize(suffix: string = ":")
+{
+	info("Randomize test order%s", suffix);
 	info("\tUse 0 for smart mode.");
 	info("\tArg:    --hasty-batch-size X");
 	info("\tConfig: hastyBatchSize=X");

src/deqp/config/args.volt
@@ -17,7 +17,7 @@ import deqp.config.info;
 fn parseArgs(settings: Settings, args: string[])
 {
 	printFailing: bool;
-	threads, hastyBatchSize: i32;
+	threads, hastyBatchSize, randomize: i32;
 	ctsBuildDir, resultsFile, tempDir, regressionFile: string;
 	testNamesFiles, regressionFiles: string[];
 
@@ -28,6 +28,7 @@ fn parseArgs(settings: Settings, args: string[])
 	watt.getopt(ref args, "results-file", ref resultsFile);
 	watt.getopt(ref args, "temp-dir", ref tempDir);
 	watt.getopt(ref args, "print-failing", ref printFailing);
+	watt.getopt(ref args, "randomize", ref randomize);
 	watt.getopt(ref args, "check|regression-file", ref regressionFiles);
 
 	if (threads > 0) {
@@ -35,6 +36,9 @@ fn parseArgs(settings: Settings, args: string[])
 	}
 	if (hastyBatchSize > 0) {
 		settings.hastyBatchSize = cast(u32) hastyBatchSize;
+	}
+	if (randomize > 0) {
+		settings.randomize = cast(u32) randomize;
 	}
 	if (ctsBuildDir !is null) {
 		settings.ctsBuildDir = ctsBuildDir;

src/deqp/driver.volt
@@ -39,6 +39,8 @@ public:
 
 	printFailing: bool = false;
 
+	randomize: u32;
+
 	threads: u32;
 
 	tempDir: string = "/tmp/dEQP";

commit 61775bfa8326a383af99ece7e03f719ff5ed9b44

deqp: print correct info in printRandomize()
Correct the copy/paste mistakes - there is no config for randomize and
the argument isn't called hasty-batch-size ;-)

Fixes: 359d0e3 ("deqp: Add --randomize argument")
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

src/deqp/config/info.volt
@@ -113,8 +113,7 @@ fn printRandomize(suffix: string = ":")
 {
 	info("Randomize test order%s", suffix);
 	info("\tUse 0 for smart mode.");
-	info("\tArg:    --hasty-batch-size X");
-	info("\tConfig: hastyBatchSize=X");
+	info("\tArg:    --randomize X");
 }
 
 fn printCtsBuildDir(suffix: string = ":")